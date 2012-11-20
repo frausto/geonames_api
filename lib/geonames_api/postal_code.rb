2faf3a7aa1718f8836de69888e9b45fc636f768f
Łukasz Tackowiak <lukasz.tackowiak@polcode.com>
2012-11-20 15:13:23 +0100

    Added some shortcut methods to PostalCode object.

--- lib/geonames_api/postal_code.rb
+++ lib/geonames_api/postal_code.rb
@@ -4,6 +4,18 @@ module GeoNamesAPI
     METHOD = "postalCodeLookupJSON"
     ID = ["postalcode", "country"]
 
+    def city
+      place_name
+    end
+
+    def state
+      admin_name1
+    end
+
+    def state_short
+      admin_code1
+    end
+
     def nearby_codes(radius=10, maxRows=5)
       NearbyPostalCode.all(postalcode, country_code, radius, maxRows)
     end
