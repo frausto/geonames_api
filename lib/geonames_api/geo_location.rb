7c917a3df1246270580bc0fad26600b4d0094e3e
Nolan Frausto <nrfrausto@gmail.com>
2012-11-21 23:00:34 -0500

    add geo location object for uses of geonameId

---
+++ lib/geonames_api/geo_location.rb
@@ -0,0 +1,17 @@
+module GeoNamesAPI
+  class GeoLocation
+    attr_accessor :geonameId
+
+    def initialize(geonameId)
+      @geonameId = geonameId
+    end
+
+    def children
+      Endpoint::Children.find(geonameId)
+    end
+
+    def hierarchy
+      Endpoint::Hierarchy.find(geonameId)
+    end
+  end
+end
---
+++ spec/geonames_api/geo_location_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe GeoNamesAPI::GeoLocation do
+
+  let(:point) { GeoNamesAPI::GeoLocation.new(2593110) }  
+
+  {
+    'children' => GeoNamesAPI::Endpoint::Children,
+    'hierarchy' => GeoNamesAPI::Endpoint::Hierarchy
+  }.each do |test_method, test_class|
+    describe "##{test_method}" do
+      it "should find #{test_method}" do
+        result = point.send(test_method.to_sym)
+        result.should be_kind_of(test_class)
+        result.geonames.size.should > 0
+      end
+    end
+  end
+end
